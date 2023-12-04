9489ed83d39f785419379d92612ed1fff7537e92
Usama-Speaks <usamaspeaks99@gmail.com>
2023-12-04 10:47:16 +0500

    usama@sqx

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import './App.css';
-import Navbar from './Components/Navbar/Navbar';
-import Videos from './Components/Navbar/Videos';
-import About from './Components/Navbar/About';
-import Roadmap from './Components/Navbar/Roadmap';
-import Meet from './Components/Navbar/Meet';
-import Footer from './Components/Navbar/Footer';
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import Videos from "./Components/Navbar/Videos";
+import About from "./Components/Navbar/About";
+import Roadmap from "./Components/Navbar/Roadmap";
+import Meet from "./Components/Navbar/Meet";
+import Footer from "./Components/Navbar/Footer";
 function App() {
   return (
-    <div >
-       <Navbar/>
-       <Videos/>
-       <About/>
-       <Roadmap/>
-       <Meet/>
-       <Footer/>
-       </div>
+    <div>
+      <Navbar />
+      <Videos />
+      <About />
+      <Roadmap />
+      <Meet />
+      <Footer />
+      <Footer />
+    </div>
   );
 }
 
